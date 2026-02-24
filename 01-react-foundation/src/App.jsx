990ad0d40a252200c251f63565dfc713c7525932
afrandormd <afrandormd@gmail.com>
2026-02-24 14:48:46 +0700

    feat(react-foundation): create multi counter component

--- 01-react-foundation/src/App.jsx
+++ 01-react-foundation/src/App.jsx
@@ -1,73 +1,11 @@
-import { useState } from 'react'
 import './App.css'
+// import MultiCounter from './components/MultiCounter'
 
 function App() {
-    const [countPlus, setPlusCount] = useState(0)
-    const [countMinus, setMinusCount] = useState(0)
-    const resetAll = () => {
-        setPlusCount(0)
-        setMinusCount(0)
-    }
 
     return (
         <>
-            <h1>Hello World dari Rando!</h1>
-            <p>Ini adalah aplikasi React pertama saya 🚀</p>
-
-            <div style={{ marginTop: '24px' }}>
-                <p>Plus Counter: <strong>{countPlus}</strong></p>
-                <p>Minus Counter: <strong>{countMinus}</strong></p>
-
-                <button onClick={() => setPlusCount(countPlus + 1)} style={{
-                    padding: '12px 24px',
-                    fontSize: '16px',
-                    background: '#61dafb',
-                    border: 'none',
-                    borderRadius: '8px',
-                    cursor: 'pointer',
-                    marginTop: '12px'
-                }}>+</button>
-                <button onClick={() => setMinusCount(countMinus - 1)} style={{
-                    padding: '12px 24px',
-                    marginLeft: '12px',
-                    fontSize: '16px',
-                    background: '#61dafb',
-                    border: 'none',
-                    borderRadius: '8px',
-                    cursor: 'pointer',
-                    marginTop: '12px'
-                }}>-</button>
-                <button onClick={() => setPlusCount(0)} style={{
-                    padding: '12px 24px',
-                    marginLeft: '12px',
-                    fontSize: '16px',
-                    background: 'orange',
-                    border: 'none',
-                    borderRadius: '8px',
-                    cursor: 'pointer',
-                    marginTop: '12px'
-                }}>Reset Plus</button>
-                <button onClick={() => setMinusCount(0)} style={{
-                    padding: '12px 24px',
-                    marginLeft: '12px',
-                    fontSize: '16px',
-                    background: 'orange',
-                    border: 'none',
-                    borderRadius: '8px',
-                    cursor: 'pointer',
-                    marginTop: '12px'
-                }}>Reset Minus</button>
-                <button onClick={() => resetAll()} style={{
-                    padding: '12px 24px',
-                    marginLeft: '12px',
-                    fontSize: '16px',
-                    background: 'red',
-                    border: 'none',
-                    borderRadius: '8px',
-                    cursor: 'pointer',
-                    marginTop: '12px'
-                }}>Reset All</button>
-            </div>
+            // <MultiCounter />
         </>
     )
 }
---
+++ 01-react-foundation/src/components/MultiCounter/index.tsx
@@ -0,0 +1,72 @@
+import { useState } from "react"
+
+export default function MultiCounter() {
+    const [countPlus, setPlusCount] = useState(0)
+    const [countMinus, setMinusCount] = useState(0)
+    const resetAll = () => {
+        setPlusCount(0)
+        setMinusCount(0)
+    }
+    return (
+        <div>
+            <h1>Hello World dari Rando!</h1>
+            <p>Ini adalah aplikasi React pertama saya 🚀</p>
+
+            <div style={{ marginTop: '24px' }}>
+                <p>Plus Counter: <strong>{countPlus}</strong></p>
+                <p>Minus Counter: <strong>{countMinus}</strong></p>
+
+                <button onClick={() => setPlusCount(countPlus + 1)} style={{
+                    padding: '12px 24px',
+                    fontSize: '16px',
+                    background: '#61dafb',
+                    border: 'none',
+                    borderRadius: '8px',
+                    cursor: 'pointer',
+                    marginTop: '12px'
+                }}>+</button>
+                <button onClick={() => setMinusCount(countMinus - 1)} style={{
+                    padding: '12px 24px',
+                    marginLeft: '12px',
+                    fontSize: '16px',
+                    background: '#61dafb',
+                    border: 'none',
+                    borderRadius: '8px',
+                    cursor: 'pointer',
+                    marginTop: '12px'
+                }}>-</button>
+                <button onClick={() => setPlusCount(0)} style={{
+                    padding: '12px 24px',
+                    marginLeft: '12px',
+                    fontSize: '16px',
+                    background: 'orange',
+                    border: 'none',
+                    borderRadius: '8px',
+                    cursor: 'pointer',
+                    marginTop: '12px'
+                }}>Reset Plus</button>
+                <button onClick={() => setMinusCount(0)} style={{
+                    padding: '12px 24px',
+                    marginLeft: '12px',
+                    fontSize: '16px',
+                    background: 'orange',
+                    border: 'none',
+                    borderRadius: '8px',
+                    cursor: 'pointer',
+                    marginTop: '12px'
+                }}>Reset Minus</button>
+                <button onClick={() => resetAll()} style={{
+                    padding: '12px 24px',
+                    marginLeft: '12px',
+                    fontSize: '16px',
+                    background: 'red',
+                    border: 'none',
+                    borderRadius: '8px',
+                    cursor: 'pointer',
+                    marginTop: '12px'
+                }}>Reset All</button>
+            </div>
+        </div>
+    )
+}
+
